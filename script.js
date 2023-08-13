e56e3ed0446eeb65a2b95f1e06db15b365fc6fc0
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-13 17:58:35 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const url = 'https://api.github.com/markdown';
+var converter = new showdown.Converter();
 
-let data = '{"text":"Hello **world**"}';
-
-async function test() {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: data,
-  });
-
-  const text = await response.text();
-  console.log(text);
-}
-
-var converter = new showdown.Converter(),
-  text = '# hello, markdown!',
+function convert(text) {
   html = converter.makeHtml(text);
-console.log(html);
+  console.log(html);
+}
 
 async function fetchReadme() {
   fetch('https://raw.githubusercontent.com/VatsaDev/VatsaDev/main/README.md')
     .then((response) => response.text())
     .then((result) => {
-      data = `{"text":${result}}`;
-      console.log(result);
+      convert(result);
     });
 }
 
