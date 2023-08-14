e2b1e0f8e1b836aee8b217803080ea049242c0e9
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-14 10:32:04 -0500

    2 files modified

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ function setup() {
   document.getElementById('userName').innerHTML = `@${u}`;
 }
 
+async function githubConvert(data) {
+  const response = await fetch('https://api.github.com/markdown', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `{"text":${data}}`,
+  });
+  console.log(response.text());
+}
+
+githubConvert('Hello **world**');
+
+const text = await response.text();
+
+console.log(text);
+
 function convert(text) {
   var html = converter.makeHtml(text);
   document.getElementById('content').innerHTML = html;
@@ -16,18 +35,17 @@ async function fetchReadme() {
   fetch(`https://raw.githubusercontent.com/${u}/${u}/master/README.md`)
     .then((response) => response.text())
     .then((result) => {
-      convert(result);
-    });
-}
-
-async function settings() {
-  fetch(`https://raw.githubusercontent.com/vatsadev/vatsadev/master/set.json`)
-    .then((response) => response)
-    .then((result) => {
-      console.log(result);
+      if ((result = '404: Not Found')) {
+        fetch(`https://raw.githubusercontent.com/${u}/${u}/main/README.md`)
+          .then((response) => response.text())
+          .then((result) => {
+            convert(result);
+          });
+      } else {
+        convert(result);
+      }
     });
 }
 
 setup();
 fetchReadme();
-settings(); // not working
